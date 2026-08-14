85fae93bb8b046ea8f02c3a3f5771857fc82c2ef
joeldl447-gif <joeldl447@gmail.com>
2026-08-14 16:53:37 -0300

    Add files via upload

--- README.txt
+++ README.txt
@@ -1,7 +1,1 @@
-ESCRITOR V3
-- Fuerza una nueva versión de la PWA sin borrar datos de Chrome.
-- Menú de capítulos mejorado.
-- Importación Word visible dentro del menú de capítulos.
-- Exportación Word.
-- Notas por párrafo.
-- Los datos existentes de Escritor se conservan en localStorage.
+V4: modo nocturno + eliminar capítulos con DOS confirmaciones consecutivas.
--- sw.js
+++ sw.js
@@ -1,11 +1,1 @@
-const CACHE='escritor-v3';
-const ASSETS=['./','./index.html?v=3','./manifest.json?v=3'];
-self.addEventListener('install',e=>self.skipWaiting());
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch',e=>{
-  e.respondWith(fetch(e.request).then(r=>{
-    const copy=r.clone();
-    caches.open(CACHE).then(c=>c.put(e.request,copy));
-    return r;
-  }).catch(()=>caches.match(e.request)));
-});
+const CACHE='escritor-v4';self.addEventListener('install',e=>self.skipWaiting());self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{let c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match(e.request))));
