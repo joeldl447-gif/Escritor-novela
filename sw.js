76e39eea504a5d9460495a2fbc3c1319f7c9e745
joeldl447-gif <joeldl447@gmail.com>
2026-08-16 13:02:04 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='escritor-v49';
+const CACHE='escritor-v50';
 self.addEventListener('install',e=>self.skipWaiting());
 self.addEventListener('activate',e=>e.waitUntil(e.clients.claim()));
 self.addEventListener('fetch',e=>e.respondWith(
